12d39f2fe451d798a9968e64b1feaecda8044683
Berachem Markria <berachem.markria@gmail.com>
2022-09-23 23:26:38 +0200

    proper php

--- class/converterJSON.php
+++ class/converterJSON.php
@@ -1,14 +1,36 @@
 <?php
 
+
+/*
+
+Renvoie une liste de chaines de chaine de caractères
+correspondant aux noms prénom des personnes 
+
+*/
+
 function getListNamebyList($list){
     $listName = array();
     foreach ($list as $pers) {
-        if (!empty($pers)) {
-            array_push($listName, $pers["nom"]." ".$pers["prenom"]);
-        }
-        
+        array_push($listName, $pers["nom"]." ".$pers["prenom"]);
     }
     return $listName;
+}
+
+/*
+
+Renvoie une liste avec les versions 'fr' des éléments s'ils 
+existent, sinon si les versions 'en' sinon rien ("")
+
+*/
+function getFrenchOrEnglishVersion($list){
+
+    if ( array_key_exists("fr",$list)){ // ARRAY
+        return  $list["fr"];
+    }elseif(array_key_exists('en',$list)){
+        return $list['en'];
+    }else{
+        return '';
+    }
 }
 
 
@@ -16,56 +38,41 @@ function getListNamebyList($list){
 $json = file_get_contents('extract_theses.json');
 $data = json_decode($json, true);
 
-//$r = new Connexion();
-
 
 foreach ($data as $key => $value) {
     $langue = $value['langue'] ;
     $title = $value['titres'][$langue];
     $author = $value['auteurs'][0]['nom'].' '.$value['auteurs'][0]['prenom'];
     $date = $value['date_soutenance'];
-
-    if ( in_array("fr",$value['resumes'])){
-        $description = $value['resumes'][$langue];
-    }elseif(in_array("en",$value['resumes'])){
-        $description = $value['resumes']["en"];
-    }else{
-        $description = '';
-    }
+    $description = getFrenchOrEnglishVersion($value['resumes']);
     $etablissement = $value['etablissements_soutenance'][0]['nom'];
     $oai_set_specs = $value['oai_set_specs'];
     $embargo = $value['embargo'];
-    
     $id = $value['nnt'];
     $link = "https://www.theses.fr/".$id;
     $director = getListNamebyList($value['directeurs_these']);// ARRAY
-    $president = getListNamebyList($value['president_jury']); // ARRAY
+    $president = sizeof($value['president_jury'])>0 ? array($value['president_jury']['nom']." ".$value['president_jury']['prenom']) : array(); // ARRAY
     $rapportors = getListNamebyList($value['rapporteurs']); // ARRAY
     $members = getListNamebyList($value['membres_jury']); // ARRAY
     $discipline = $value['discipline']['fr'];
     $status = $value['status'];
+    $subjects = getFrenchOrEnglishVersion($value['sujets']);
 
-    print_r($value['directeurs_these']);
-
-
-    if ( in_array("fr",$value['sujets'])){ // ARRAY
-        $subjects = $value['sujets']["fr"];
-    }elseif(in_array('en',$value['sujets'])){
-        $subjects = $value['sujets']['en'];
-    }else{
-        $subjects = '';
-    }
-    echo "nnt : ".$id ." titre : ".$title." auteur : ".$author." date : ".$date." description : ".$description." etablissement : ".$etablissement."  embargo : ".$embargo." discipline : ".$discipline." status : ".$status." link : ".$link.'<br>';
-    print_r($director).'<br>'. print_r($president).'<br>'. print_r($rapportors).'<br>'. print_r($members).'<br>'. print_r($subjects).'<br>';
-
+    echo 'description : ';
+    echo "id : ".$id."<br>";
+    print_r($director);
+    echo '<br>';
+    print_r($president);
+    echo '<br>';
+    print_r($rapportors);
+    echo '<br>';
+    print_r($members);
+    echo '<br>';
+    print_r($subjects);
+    echo '<br>';
+    echo "nnt : ".$id ."<br> titre : ".$title."<br> auteur : ".$author."<br> date : ".$date."<br> description : ".$description."<br> etablissement : ".$etablissement."<br> discipline : ".$discipline."<br> status : ".$status."<br><br>";
     echo "<br><br>" ;
 }
-
-
-//$r->q("SELECT * FROM theses");
-
-
-//print_r($r);
 
 
 
